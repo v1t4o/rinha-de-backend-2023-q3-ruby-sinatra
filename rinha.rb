690b4efdde90ebe9ae8efd8c63ba7df8b5acfb8e
Victor de Oliveira <vitor.oliveira.marinho@gmail.com>
2023-08-17 21:52:49 -0300

    feat: create initial routes

---
+++ rinha.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+
+post '/pessoas' do
+  'Put this in your pipe & smoke it!'
+end
+
+get '/pessoas/:id' do
+  'Put this in your pipe & smoke it!'
+end
+
+get '/pessoas' do
+  'Put this in your pipe & smoke it!'
+end
+
+get '/contagem-pessoas' do
+  'Put this in your pipe & smoke it!'
+end
